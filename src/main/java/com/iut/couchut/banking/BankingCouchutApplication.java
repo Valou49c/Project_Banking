7fd0d1816f663788072e786cf005be5c2cb37ba2
Valou49c <Valou49c@gmail.com>
2015-02-07 23:54:57 +0100

    Initial commit

---
+++ src/main/java/com/iut/couchut/banking/BankingCouchutApplication.java
@@ -0,0 +1,12 @@
+package com.iut.couchut.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingCouchutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingCouchutApplication.class, args);
+    }
+}
